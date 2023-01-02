ceed498882594c53a35de58aab36c9762bc59210
Charles Edge <krypted@me.com>
2023-01-02 16:19:18 -0500

    Add files via upload

---
+++ extensionsman-main/extensionsman/Filter.swift
@@ -0,0 +1,25 @@
+//
+//  Filter.swift
+//
+// Created by Charles Edge
+//
+
+import Foundation
+
+protocol Filter {
+    func isValid(_ value: String) -> Bool
+}
+
+class AppleExtensionFilter: Filter {
+    
+    func isValid(_ value: String) -> Bool {
+        value.starts(with: "com.apple") && !value.isEmpty
+    }
+}
+
+class ThirdPartyExtensionFilter: Filter {
+    
+    func isValid(_ value: String) -> Bool {
+        !value.starts(with: "com.apple") && !value.isEmpty
+    }
+}
---
+++ extensionsman-main/extensionsman/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//
+// Created by Charles Edge
+//
+
+import Foundation
+
+let extensionMan = ExtensionMan()
+extensionMan.staticMode()
